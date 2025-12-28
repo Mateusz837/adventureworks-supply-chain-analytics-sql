9f69a78b20ad5a9a76ac1776677601f18c850c77
Mateusz837 <mateusz.kozlinski@gmail.com>
2025-12-28 11:01:53 +0100

    Update 01_inventory_supply_chain.sql

--- sql/01_inventory_supply_chain.sql
+++ sql/01_inventory_supply_chain.sql
@@ -52,4 +52,47 @@ INNER JOIN Production.Location AS l
 WHERE p.StandardCost > 0
 ORDER BY Inventory_Value DESC;
 
+-- Task 1.4 — Inventory Turnover
+-- Business purpose: Measure how many times inventory is sold and replenished over a year.
+-- How this helps: Highlights slow-moving vs fast-rotating SKUs to optimize replenishment, reduce overstock, and improve working capital.
+-- Assumption: Uses last 12 months sales vs current inventory because AdventureWorks does not provide historical inventory snapshots.
+
+WITH Date_Range AS (
+    SELECT DATEADD(YEAR, -1, MAX(OrderDate)) AS StartDate, MAX(OrderDate) AS EndDate
+    FROM Sales.SalesOrderHeader
+),
+SalesTab AS (
+    SELECT sd.ProductID, p.Name, p.ProductNumber, SUM(sd.OrderQty) AS Total_Qt
+    FROM Sales.SalesOrderDetail sd
+    INNER JOIN Sales.SalesOrderHeader sh
+        ON sd.SalesOrderID = sh.SalesOrderID
+    INNER JOIN Production.Product p
+        ON sd.ProductID = p.ProductID
+    WHERE sh.OrderDate BETWEEN (SELECT StartDate FROM Date_Range) AND (SELECT EndDate FROM Date_Range)
+    GROUP BY sd.ProductID, p.Name, p.ProductNumber
+),
+Inventory AS (
+    SELECT ProductID, SUM(Quantity) AS Inventory_QT
+    FROM Production.ProductInventory
+    GROUP BY ProductID
+),
+Turnover_Tab AS (
+    SELECT st.ProductID, st.Name, st.ProductNumber, st.Total_Qt AS Sales_Qty, i.Inventory_QT,
+           CASE WHEN i.Inventory_QT = 0 THEN NULL
+                ELSE CAST(st.Total_Qt / i.Inventory_QT AS DECIMAL(10,2))
+           END AS Turnover
+    FROM SalesTab st
+    INNER JOIN Inventory i
+        ON st.ProductID = i.ProductID
+)
+SELECT
+    ProductID, Name, ProductNumber, Sales_Qty, Inventory_QT, Turnover,
+    CASE WHEN Turnover < 1 THEN 'Slow_Rotation_Risk'
+         WHEN Turnover <= 4 THEN 'Mid_Rotation'
+         WHEN Turnover > 4 THEN 'Quick_Rotation'
+    END AS Rotation
+FROM Turnover_Tab
+ORDER BY Turnover DESC;
+
+
 
